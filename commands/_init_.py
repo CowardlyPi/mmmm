d65c9a012abb79333faaaf418554e4bd7547c5b1
CowardlyPi <ethancuellar122@gmail.com>
2025-05-17 11:22:50 -0700

    Update _init_.py

--- commands/_init_.py
+++ commands/_init_.py
@@ -1,5 +1,6 @@
 # commands/__init__.py
 from commands.user_commands import setup_user_commands
 from commands.admin_commands import setup_admin_commands
+from commands.enhanced_commands import setup_enhanced_commands
 
-__all__ = ['setup_user_commands', 'setup_admin_commands']
+__all__ = ['setup_user_commands', 'setup_admin_commands', 'setup_enhanced_commands']
