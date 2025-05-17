4f573e033d9707860687a6b1aa09fa0536bd7a0a
CowardlyPi <ethancuellar122@gmail.com>
2025-05-17 13:31:11 -0700

    Update _init_.py

--- models/_init_.py
+++ models/_init_.py
@@ -1,4 +1,17 @@
 # models/__init__.py
 from models.user_profile import UserProfile
+from models.database import (
+    User, UserEmotions, UserMemory, UserEvent, UserMilestone,
+    UserProfile as DBUserProfile, Conversation, ConversationSummary, 
+    DMSettings, InteractionStats, RelationshipProgress,
+    init_db, get_session_factory
+)
 
-__all__ = ['UserProfile']
+__all__ = [
+    'UserProfile',
+    # Database models
+    'User', 'UserEmotions', 'UserMemory', 'UserEvent', 'UserMilestone',
+    'DBUserProfile', 'Conversation', 'ConversationSummary', 
+    'DMSettings', 'InteractionStats', 'RelationshipProgress',
+    'init_db', 'get_session_factory'
+]
