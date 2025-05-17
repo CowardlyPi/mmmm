613d4aeb8f2e8d9786fa94a166acd012680704ef
CowardlyPi <ethancuellar122@gmail.com>
2025-05-17 13:28:44 -0700

    Update _init_.py

--- managers/_init_.py
+++ managers/_init_.py
@@ -3,5 +3,12 @@ from managers.conversation import ConversationManager
 from managers.emotion import EmotionManager
 from managers.storage import StorageManager
 from managers.response import ResponseGenerator
+from managers.postgres_storage import PostgreSQLStorageManager
 
-__all__ = ['ConversationManager', 'EmotionManager', 'StorageManager', 'ResponseGenerator']
+__all__ = [
+    'ConversationManager', 
+    'EmotionManager', 
+    'StorageManager', 
+    'ResponseGenerator',
+    'PostgreSQLStorageManager'
+]
